87c016133299071fef96be6939c5fbde27258676
wenge8n <wenge8n@outlook.com>
2022-12-15 11:36:49 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oiler-capacitor-ble-sdk-demo-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
